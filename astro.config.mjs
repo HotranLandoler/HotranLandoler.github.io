b3ad0b1ff411e7609d475c06304c91ff5b9cb572
LOU Chaoli <642600539@qq.com>
2024-02-09 14:26:50 +0800

    Upgrade to Astro 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
+import icon from "astro-icon";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(), svelte()],
+  integrations: [astroI18next(), svelte(), icon()],
   site: "https://HotranLandoler.github.io",
   scopedStyleStrategy: "class",
 });
