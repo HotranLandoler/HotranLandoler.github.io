c9e015a25c4aae3c72ebcf1495d538a98adf24e5
LOU Chaoli <642600539@qq.com>
2024-02-09 14:47:39 +0800

    Use astro sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,26 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 import icon from "astro-icon";
-
 import svelte from "@astrojs/svelte";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next(), svelte(), icon()],
-  site: "https://HotranLandoler.github.io",
+  integrations: [
+    astroI18next(),
+    svelte(),
+    icon(),
+    sitemap({
+      i18n: {
+        defaultLocale: "zh", // 所有不包含 `es` 或 `fr` 的链接都将被视为默认语言环境，即 `en`
+        locales: {
+          zh: "zh", // `defaultLocale` 的值必须在 `locales` 键中存在
+          en: "en",
+        },
+      },
+    }),
+  ],
+  site: "https://chaoli.io",
   scopedStyleStrategy: "class",
 });
