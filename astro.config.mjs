412751faa7522d176050c34125d86df7b793ff65
Chaoli <ex012@qq.com>
2025-10-11 14:19:36 +0800

    feat: remake main page layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import icon from 'astro-icon';
 import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from '@astrojs/tailwind';
 import { locales, defaultLocale, localesAsObject } from './src/i18n';
 
 // https://astro.build/config
@@ -17,12 +16,8 @@ export default defineConfig({
         locales: localesAsObject,
       },
     }),
-    tailwind({
-      applyBaseStyles: false,
-    }),
   ],
   site: 'https://chaoli.io',
-  scopedStyleStrategy: 'class',
   i18n: {
     locales: locales,
     defaultLocale: defaultLocale,
